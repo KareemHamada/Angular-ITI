7da19b017127088cb9049e22436c9c4cb8db1286
KH <KH@DESKTOP-KE662S4>
2023-04-14 02:38:39 +0200

    Components Interaction - @Input

--- src/app/Directives/light-box.directive.ts
+++ src/app/Directives/light-box.directive.ts
@@ -1,21 +1,26 @@
-import { Directive, ElementRef, HostListener,Input } from '@angular/core';
+import { Directive, ElementRef, HostListener,Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Directive({
   selector: '[LightBox]'
 })
-export class LightBoxDirective {
-  @Input('LightBox') defaultColor: string = "yellow";
+export class LightBoxDirective implements OnChanges{
+  @Input('LightBox') highlightColor: string = "yellow";
+  @Input() defaultColor: string = "yellow";
+
   constructor(private ele:ElementRef) {
-    this.ele.nativeElement.style.border = "2px solid red";
-    this.ele.nativeElement.style.color = "red";
+    // this.ele.nativeElement.style.border = `2px solid ${this.defaultColor}`;
+    // this.ele.nativeElement.style.color = "red";
+  } 
+  ngOnChanges(): void {
+    this.ele.nativeElement.style.border = `2px solid ${this.defaultColor}`;
   }
 
   @HostListener('mouseover') onMouseOver(){
-    this.ele.nativeElement.style.border = `2px solid ${this.defaultColor}`;
+    this.ele.nativeElement.style.border = `2px solid ${this.highlightColor}`;
 
   }
   @HostListener('mouseout') onMouseOut(){
-    this.ele.nativeElement.style.border = "2px solid blue"
+    this.ele.nativeElement.style.border = `2px solid ${this.defaultColor}`;
 
   }
 }
